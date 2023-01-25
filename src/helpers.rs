e6488c25431fb8d0078215c3000b7fd6584d33f0
theMackabu <theMackabu@gmail.com>
2023-01-24 17:56:12 -0800

    [release v0.2.1] find maidfile in parent dir

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,5 +1,20 @@
 use just_macros::{crashln, errorln};
-use std::fs;
+use std::{env, fs, path::Path, path::PathBuf};
+
+pub fn find_file(starting_directory: &Path, filename: &String) -> Option<PathBuf> {
+    let mut path: PathBuf = starting_directory.into();
+    let file = Path::new(filename);
+
+    loop {
+        path.push(file);
+        if path.is_file() {
+            break Some(path);
+        }
+        if !(path.pop() && path.pop()) {
+            break None;
+        }
+    }
+}
 
 pub fn string_to_static_str(s: String) -> &'static str {
     Box::leak(s.into_boxed_str())
@@ -10,11 +25,25 @@ pub fn value_error(debug_err: &str) {
     errorln!("Unable to parse maidfile. Does it contain non string values?");
 }
 
-pub fn read_maidfile(path: &String) -> String {
-    match fs::read_to_string(path) {
-        Ok(contents) => format!("[env]\n{contents}"),
-        Err(_) => {
-            crashln!("Cannot find maidfile. Does it exist?");
+pub fn read_maidfile(filename: &String) -> String {
+    match env::current_dir() {
+        Ok(path) => match find_file(&path, &filename) {
+            Some(path) => {
+                log::info!("Found maidfile path: {}", path.display());
+                match fs::read_to_string(path) {
+                    Ok(contents) => format!("[env]\n{contents}"),
+                    Err(_) => {
+                        crashln!("Cannot find maidfile. Does it exist?");
+                    }
+                }
+            }
+            None => {
+                crashln!("Cannot find maidfile. Does it exist?");
+            }
+        },
+        Err(err) => {
+            log::warn!("{err}");
+            crashln!("Home directory could not found.");
         }
     }
 }
